9f5ae262ef58f354e99e7c565b91a2ae5c6b9753
Mykola Matsko <mykola.matsko@gmail.com>
2020-05-29 16:02:59 +0300

    Creating and Combining Views

---
+++ 1.Apple-Developer-iOS/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  1.Apple-Developer-iOS
+//
+//  Created by Mykola Matsko on 29.05.2020.
+//  Copyright © 2020 Mykola Matsko. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
